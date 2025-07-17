61e48f267c38e1b5bac92a0943eeecc94984307c
Naveen Kumar S <152676387+nsk2k04@users.noreply.github.com>
2025-07-17 22:23:51 +0530

    Week4_handson

---
+++ Week4/Week4_Spring-rest-handson/Ex5_REST-countrybasedoncountrycode/spring-country-service-in/src/test/java/com/cognizant/spring_country_service_in/SpringCountryServiceInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_country_service_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCountryServiceInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
