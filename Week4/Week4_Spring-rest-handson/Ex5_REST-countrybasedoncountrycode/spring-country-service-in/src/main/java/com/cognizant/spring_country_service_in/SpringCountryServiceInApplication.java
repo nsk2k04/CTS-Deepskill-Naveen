fa33573d7ab8526e91e0f5f62736fe490ea2fb69
Naveen Kumar S <naveenkumarsampath2004@gmail.com>
2025-08-08 10:42:20 +0530

    Add all files

---
+++ Week4/Week4_Spring-rest-handson/Ex5_REST-countrybasedoncountrycode/spring-country-service-in/src/main/java/com/cognizant/spring_country_service_in/SpringCountryServiceInApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.spring_country_service_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCountryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCountryServiceApplication.class, args);
+    }
+}
